9843b93055352ac91a57c6dd143696558b92ea9e
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2023-01-11 11:59:52 +0500

    Add files via upload

--- src/comp/Box.js
+++ src/comp/Box.js
@@ -2,18 +2,19 @@ import "./Box.css";
 import React,{useState} from "react";
 const Box = (props) => {
   const [ButtonSubmit, setButtonSubmit] = useState('');
+  const [data,setData] = useState(ButtonSubmit)
   //this is Function is not working
-  const ButtonHandler = (event) =>{
-    // setButtonSubmit(props.Handler);
-    // console.log(ButtonSubmit);
-    // console.log('working');
-    setButtonSubmit(event.target.value);
+  const buttonSubmit = (event)=>{
+    setButtonSubmit(event.target.value)
   }
-    return (<div>
+  const ButtonHander = ()=>{
+    setData(ButtonSubmit)
+  }
+     return (<div>
     
-<span><label style={{borderColor:'red'}} className="label">Enter Your Color:</label></span> 
-    <input type="text"  className={`textfiled ${!props.IsValid ? 'invalid' : ''}`} onChange={ButtonSubmit}></input>
-    <button onClick={ButtonHandler}>Button</button>
+<span><label style={{borderColor:'red'}} className="label">Enter Your Color:{data}</label></span> 
+    <input type="text"  className={`textfiled ${!props.IsValid ? 'invalid' : ''}`} onChange={buttonSubmit}></input>
+    <button onClick={ButtonHander}>Button</button>
     </div>
   );
 };
